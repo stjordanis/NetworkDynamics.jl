44312c9de9ff5dd5b1c968a2387276dab979984b
lindnemi <michaellindner@pik-potsdam.de>
2020-06-18 17:47:40 +0200

    included DDE test setup, no DDE tests so far

--- test/NDFunctions_test.jl
+++ test/NDFunctions_test.jl
@@ -25,19 +25,51 @@ end
     nothing
 end
 
+@inline function kuramoto_delay_edge!(e, v_s, v_d, h_v_s, h_v_d, p, t)
+    # The coupling is no longer symmetric, so we need to store BOTH values (see tutorials for details)
+    e[1] = p * sin(v_s[1] - h_v_d[1])
+    e[2] = p * sin(h_v_s[1] - v_d[1])
+    nothing
+end
+
+@inline function kuramoto_delay_vertex!(dv, v, e_s, e_d, h_v, p, t)
+    dv[1] = p
+    for e in e_s
+        dv[1] -= e[1]
+    end
+    for e in e_d
+        dv[1] -= e[2]
+    end
+    nothing
+end
+
 
 odevertex = ODEVertex(f! = diffusion_vertex!, dim = 1)
 staticvertex = StaticVertex(f! = diff_stat_vertex!, dim = 1)
 staticedge = StaticEdge(f! = diffusion_edge!, dim = 1)
 odeedge = ODEEdge(f! = diff_dyn_edge!, dim = 1)
+# dim of the edge is 2 since the coupling is not symmetric
+sdedge = StaticDelayEdge(f! = kuramoto_delay_edge!, dim = 2)
+ddevertex = DDEVertex(f! = kuramoto_delay_vertex!, dim = 1)
+
+
 
 vertex_list_1 = [odevertex for v in 1:10]
 @test eltype(vertex_list_1) == ODEVertex{typeof(diffusion_vertex!)}
 
 vertex_list_2 = [staticvertex for v in 1:10]
+@test eltype(vertex_list_2) == StaticVertex{typeof(diff_stat_vertex!)}
+
+
+
 vertex_list_3 = [v % 2 == 0 ? odevertex : staticvertex for v in 1:10]
 vertex_list_4 = Array{VertexFunction}(vertex_list_3)
+@test eltype(vertex_list_4) == VertexFunction
+
 vertex_list_5 = Array{ODEVertex}(vertex_list_3)
+@test eltype(vertex_list_5) == ODEVertex
+
+
 @test_throws MethodError Array{StaticVertex}(vertex_list_3) # this should error out
 
 edge_list_1 = [odeedge for v in 1:10]
